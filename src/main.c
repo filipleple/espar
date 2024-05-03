2919978303039e6fc97df30a75d31591c1f69bb0
Filip Lewiński <filiple00@protonmail.com>
2024-05-03 18:24:51 +0200

    update: 5 point scan done
    Signed-off-by: Filip Lewiński <filiple00@protonmail.com>

--- src/main.c
+++ src/main.c
@@ -8,29 +8,13 @@
 #define CHIP_I2C_ADDR		0b0100000
 #define I2C_NODE			DT_NODELABEL(i2c0)
 
-//assuming IOCON.BANK=0
-/*
+//IOCON.BANK=0
 #define IODIRA_ADDR			0x00
 #define OLATA_ADDR			0x14
 #define GPIOA_ADDR			0x12
-
 #define IODIRB_ADDR			0x01
 #define OLATB_ADDR			0x15
 #define GPIOB_ADDR			0x13
-*/
-
-//assuming IOCON.BANK=1
-
-#define IODIRA_ADDR			0x00
-#define OLATA_ADDR			0x0A
-#define GPIOA_ADDR			0x09
-
-//assuming IOCON.BANK=1
-#define IODIRB_ADDR			0x10
-#define OLATB_ADDR			0x1A
-#define GPIOB_ADDR			0x19
-
-
 
 static const struct device *i2c0_device = DEVICE_DT_GET(I2C_NODE);
 
@@ -39,125 +23,63 @@ static uint8_t i2c_data_buffer[2];
 
 static uint8_t i2c_buffer[2];
 
-uint8_t read_from_register();
-uint8_t write_to_register();
+uint8_t set_gpios_to_output();
 
 int main(void)
 {
-	int err, i;
-
+	int err = 0;
+	
 	if (!device_is_ready(i2c0_device)){
 		printk("i2c device not ready\n\r");
-		return;
+		return -1;
 	}
 
+	unsigned int combined = 0x1F;  // 111110 in binary for Register A to start with five '1's
+    const unsigned int maskA = 0x003F; // Mask for the lower 6 bits (0000000000111111)
+    const unsigned int maskB = 0x0FC0; // Mask for the next 6 bits (0000111111000000)
+    const unsigned int lastBitBMask = 0x0800; // 12th bit from LSB, the last bit of B
+
+	unsigned int regA = 0, regB = 0, lastBitB = 0;
+
+	while(1){	
+		set_gpios_to_output();
+
+		//Update GPIO registers
+		//bank a			
+		i2c_buffer[0] = GPIOA_ADDR;
+		i2c_buffer[1] = regA;
+		err = i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
+		if (err != 0) continue;
 		
-
-	while(1){
-		do{
-			//bank a
-			
-			//read from direction and state registers
-			i2c_register_buffer[0] = IODIRA_ADDR; // IODIRA: pin bank A I/O mode
-			read_from_register();
-			printk("data from register %d: %d \n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-
-			i2c_register_buffer[0] = OLATA_ADDR; // OLATA: pin bank A latch register
-			read_from_register();
-			printk("data from register %d: %d \n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-
-			i2c_register_buffer[0] = GPIOA_ADDR; // GPIOA: pin bank A GPIO register
-			read_from_register();
-			printk("data from register %d: %d \n\n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-			
-			//set direction to output (low) and pin states to high			
-			i2c_buffer[0] = IODIRA_ADDR;
-			i = i%2; 
-			if (i) i2c_buffer[1] = 0x00;
-			else i2c_buffer[1] = 0xFF;
-			i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-
-			i2c_buffer[0] = OLATA_ADDR;
-			i = i%2; 
-			if (!i) i2c_buffer[1] = 0x00;
-			else i2c_buffer[1] = 0xFF;
-			i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-
-			i2c_buffer[0] = GPIOA_ADDR;
-			i = i%2; 
-			if (!i) i2c_buffer[1] = 0x00;
-			else i2c_buffer[1] = 0xFF;
-			err = i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-			
-			//bank b
-			
-			//read from direction and state registers
-			
-			i2c_register_buffer[0] = IODIRB_ADDR; // IODIRA: pin bank B I/O mode
-			read_from_register();
-			printk("data from register %d: %d \n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-
-			i2c_register_buffer[0] = OLATB_ADDR; // OLATA: pin bank b latch register
-			read_from_register();
-			printk("data from register %d: %d \n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-
-			i2c_register_buffer[0] = GPIOB_ADDR; // GPIOA: pin bank B GPIO register
-			read_from_register();
-			printk("data from register %d: %d \n\r", i2c_register_buffer[0], i2c_data_buffer[0]);
-			
-			//set direction to output (low) and pin states to high
-			
-			i2c_buffer[0] = IODIRB_ADDR;
-			i2c_buffer[1] = 0xFF;			
-			i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-
-			i2c_buffer[0] = OLATB_ADDR;
-			i = i%2; 
-			if (!i) i2c_buffer[1] = 0x00;
-			else i2c_buffer[1] = 0xFF;
-			i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-			
-			i2c_buffer[0] = GPIOB_ADDR;
-			i = i%2; 
-			if (!i) i2c_buffer[1] = 0x00;
-			else i2c_buffer[1] = 0xFF;
+		//bank b			
+		i2c_buffer[0] = GPIOB_ADDR;
+		i2c_buffer[1] = regB;
+		err = i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
+		if (err != 0) continue;
 
 
-			i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
-
-		i++;
-
-		}while(false);
+		lastBitB = (combined & lastBitBMask) ? 1 : 0; // Check the last bit of B before shift
+        combined <<= 1;
+        // Wrap the last bit of B to the start of A if set
+        if (lastBitB) {
+            combined |= 0x01; // Set the first bit of A
+        }
+        // Extract A and B
+        regA = combined & maskA;
+        regB = (combined & maskB) >> 6; // Shift down to make it 6-bit        
 
 		k_msleep(SLEEP_TIME_MS);
 	}
-}
-
-uint8_t read_from_register(){
-	int err = 0;
-	err = i2c_write(i2c0_device, i2c_register_buffer, 1, CHIP_I2C_ADDR);
-	if (err<0){printk("failed: %n \n\r", &err);}
-
-	err = i2c_read(i2c0_device, i2c_data_buffer, 1, CHIP_I2C_ADDR);
-	if (err<0){printk("failed: %n \n\r", &err);}
 
 	return err;
 }
 
-uint8_t write_to_register(){	
-	int err = 0;
-	err = i2c_write(i2c0_device, i2c_register_buffer, 1, CHIP_I2C_ADDR);
-	if (err<0){printk("failed: %n \n\r", &err); return err;}
+uint8_t set_gpios_to_output(){
+	i2c_buffer[0] = IODIRA_ADDR;
+	i2c_buffer[1] = 0x00;
+	i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
 
-	err = i2c_write(i2c0_device, i2c_data_buffer, 1, CHIP_I2C_ADDR);
-	if (err<0){printk("failed: %n \n\r", &err); return err;}
-
-	return err;	
+	i2c_buffer[0] = IODIRB_ADDR;
+	i2c_buffer[1] = 0x00;			
+	i2c_write(i2c0_device, i2c_buffer, 2, CHIP_I2C_ADDR);
 }
-
-/*
-
-6 LSB bank 12 i 13 
-
-
-*/
